a7c6c1e2e4a78d264b53c3788cfba361059bf5bf
Aritra Biswas <aritra.biswas@nielsen.com>
2019-06-06 14:57:33 +0530

    api

---
+++ plumber.R
@@ -0,0 +1,20 @@
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+#* @serializer unboxedJSON
+function(msg){
+  df<-readr::read_csv(msg)
+  list(names(df))
+}
+
+
+#* subset data back the input
+#* @param col1 The message to col1
+#* @param col2 The message to col2
+#* @param path The message to path
+#* @get /subset
+function(path,col1,col2){
+  df<-readr::read_csv(path) %>% 
+    select(c(col1,col2))
+  list(x_axis=rnorm(10),y_axis=rnorm(10),dates=seq(as.Date('2011-01-01'),as.Date('2011-01-10'),by = 1))
+}
